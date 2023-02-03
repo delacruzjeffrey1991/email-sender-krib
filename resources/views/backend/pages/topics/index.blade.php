4d1c2badd9f68c7ee7adc576b1b92a60c3436581
Emman <emman_Agan1995@yahoo.com.ph>
2023-02-03 11:35:50 +0800

    update

--- resources/views/backend/pages/topics/index.blade.php
+++ resources/views/backend/pages/topics/index.blade.php
@@ -10,7 +10,7 @@
     <div class="container-fluid">
         @include('backend.layouts.partials.messages')
         <div class="table-responsive product-table">
-            <button class="btn btn-success" data-toggle="modal" data-target="#addEmail">Add</button>
+            <button class="btn btn-success" data-toggle="modal" data-target="#addTopics">Add Topics</button>
 
             <table class="table table-striped table-bordered display ajax_view">
                 <thead>
@@ -31,9 +31,39 @@
         </div>
     </div>
 
+
+<div id="addTopics" class="modal fade" role="dialog"  data-backdrop="static" data-keyboard="false" >
+  <div class="modal-dialog">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+      </div>
+      <div class="modal-body">
+           <div class="row ">
+         <div class="col-md-12">
+            <div class="form-group">
+                <label class="control-label" for="title">Display Name<span class="required">*</span></label>
+                <input type="text" class="form-control" id="add-display-name"  required=""/>
+            </div>
+            <div class="form-group">
+                <label class="control-label" for="title">Topic Name<span class="required">*</span></label>
+                <input type="text" class="form-control" id="add-topic-name"  required=""/>
+            </div>
+        </div>
+      </div>
+      </div>
+       <div class="modal-footer">
+        <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-default" id="saveTopic">Save</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
     <div id="viewList" class="modal fade" role="dialog"  data-backdrop="static" data-keyboard="false" >
   <div class="modal-dialog">
-
     <!-- Modal content-->
     <div class="modal-content">
       <div class="modal-header">
@@ -90,10 +120,10 @@
     $(document).on("click", ".viewContacts", function() {
             const topic = $(this).attr("data-topic")
             $("#tb-contacts").html(`<tr id="tb-contacts-loader">
-                        <td >
-                            <p class="text-center">Please wait... <i class="fa fa-spinner fa-spin"></i></p>
-                        </td>
-                    </tr>`)
+            <td >
+            <p class="text-center">Please wait... <i class="fa fa-spinner fa-spin"></i></p>
+            </td>
+            </tr>`)
 
           fetch(`{{url('api/contact/list?contact_list_name=jeffrey_contact_list')}}&topic_name=${topic}`)
           .then(response => response.json())
@@ -117,7 +147,35 @@
           .catch(error => {
             alert(JSON.stringify(error))
           });
+    })
 
+
+    $("#saveTopic").on('click', function() { 
+
+        if($("#add-display-name").val() &&  $("#add-topic-name").val()) {
+        var formdata = new FormData();
+        formdata.append("topics[0][DisplayName]", $("#add-display-name").val());
+        formdata.append("topics[0][TopicName]",  $("#add-topic-name").val());
+        formdata.append("contact_list_name", "jeffrey_contact_list");
+
+
+        var requestOptions = {
+          method: 'POST',
+          body: formdata,
+          redirect: 'follow'
+        };
+
+        fetch("{{url('/api/contact-list/add-topic')}}", requestOptions)
+          .then(response => response.json())
+          .then(result => {
+            if(result.success) {
+                window.location.reload()
+            }
+          })
+          .catch(error => console.log('error', error));
+        } else {
+            alert("Please fill up.")
+        }
 
     })
 
